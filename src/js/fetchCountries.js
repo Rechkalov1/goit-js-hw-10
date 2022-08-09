7e18868fff465c93340cf687022c5d8d2b77a035
EnVy <envy7321@gmail.com>
2022-08-09 12:48:48 +0300

    save

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,9 @@
-const URL="https://restcountries.com/v3.1"
+const URL="https://restcountries.com/v2"
 
 
 export default function fetchCountries(name){
     
     return fetch(`${URL}/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response=>response.json())
+ 
 }
